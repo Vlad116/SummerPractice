8cdcd41fbabac1ae92c3a2c5478f856919816a58
Vlad116 <vladalekseev9@yandex.ru>
2018-07-15 10:08:48 +0300

    del

--- SportBox_WEB/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const fs = require('fs');
-require('./app/routes/registration_routes')(app, fs);
-require('./app/routes/login_routes')(app, fs);
-require('./app/routes/addProduct_routes')(app, fs);
-app.use(express.static('public'));
-app.listen(8080);
-console.log("Server started at 8080");
