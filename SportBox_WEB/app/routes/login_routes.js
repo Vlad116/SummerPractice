3db931911e79377e0994478986d0ba9090cf784c
Vlad116 <vladalekseev9@yandex.ru>
2018-07-17 23:12:32 +0300

    update, fix registration, styles, and catalogue

--- SportBox_WEB/app/routes/login_routes.js
+++ SportBox_WEB/app/routes/login_routes.js
@@ -6,10 +6,8 @@ module.exports = function (app, fs) {
     app.post('/login', bodyParser, (request, response) => {
 
         var body = request.body;
-        // var notRegistered = false;
 
-        // console.log(body);
-        fs.readFile('registrationData.txt', 'utf-8', function (err, data) {
+        fs.readFile('registrationData.txt', 'utf-8',function (err, data) {
 
             var lines = data.split('\n');
 
@@ -17,7 +15,7 @@ module.exports = function (app, fs) {
             var strPass = body.password;
 
             var log = '';
-            for (var i = 0; i < lines.length; i++) {
+            for (var i = 0; i < lines.length - 1; i++) {
 
                 if (((lines[i].split(' ')[0].localeCompare(strName) === 0)
                     || (lines[i].split(' ')[1].localeCompare(strName) === 0))
@@ -25,16 +23,15 @@ module.exports = function (app, fs) {
                     log = 'Good';
                     console.log(log);
                     console.log(body.username + ' ' + body.password + ' ' + '- visit log!');
-                    // notRegistered = false;
+
                     response.redirect("/html/account.html");
                     return;
+                } else {
+
                 }
-                // 0-email
-                // 1-username
-                // 2-password
+
             }
 
-            console.log(body.username + ' ' + body.password + '\n');
             log = 'The user is not registered or you entered the data incorrectly';
             console.log(log);
             // notRegistered = true;
@@ -42,35 +39,5 @@ module.exports = function (app, fs) {
 
         });
 
-        // console.log(notRegistered);
-        // if (notRegistered) {
-        //     fs.appendFile('loginData.txt', body.username + ' ' + body.password + ' ' + body.rememberMe + '\n',
-        //         function (err) {
-        //             if (err) throw err;
-        //             console.log(body.username + ' ' + body.password + ' ' + '- visit log!');
-        //             response.redirect("/html/account.html");
-        //         });
-        // }
     });
 };
-
-
-//     app.get('/login', function (request, response) {
-//         fs.readFile('registrationData.txt', 'utf-8', function(err, data) {
-//            
-//             var lines = data.split('\n');
-//
-//             var result = [];
-//            
-//
-//
-//             for (var i = 0; i < lines.length; i++) {
-//                 result.push({'name' : lines[i].split(' ')[0],
-//                     'surname': lines[i].split(' ')[1]});
-//             }
-//
-//             response.setHeader('Content-Type', 'application/json');
-//             response.send(JSON.stringify(result));
-//         });
-//     });
-// };
--- SportBox_WEB/app/routes/registration_routes.js
+++ SportBox_WEB/app/routes/registration_routes.js
@@ -6,18 +6,44 @@ module.exports = function (app, fs) {
 
         var body = request.body;
 
-        fs.appendFile('registrationData.txt', body.email + ' ' + body.username + ' '
-            + body.confirmpassword + ' '
-            + body.firstname + ' ' + body.secondname + ' '
-            + body.city + ' ' + body.state + ' ' + body.subscribe + '\n',
-            function (err) {
-                if (err) throw err;
-                console.log(body.email + ' ' + body.username + '\n'
-                    + body.confirmpassword + '\n'
-                    + body.firstname + ' ' + body.secondname + '\n'
-                    + body.city + ' ' + body.state + '\n' + body.subscribe + '\n' + '-registrated!' + '\n');
-                response.redirect("/html/account.html");
-            });
-    });
+        fs.readFile('registrationData.txt', 'utf-8', function (err, data) {
 
+            var lines = data.split('\n');
+            var strEmail = body.email;
+            var strUsername = body.username;
+
+            var log = '';
+            for (var i = 0; i < lines.length; i++) {
+
+                if ((lines[i].split(' ')[0].localeCompare(strEmail) === 0) && (lines[i].split(' ')[1].localeCompare(strUsername) === 0)) {
+                    log = ' - This email is already registered and this username' + body.username + 'is already taken!';
+                    console.log(body.email + log);
+                    notRegistered = false;
+                    response.redirect("/html/registration.html");
+                    return;
+                } else if (lines[i].split(' ')[0].localeCompare(strEmail) === 0) {
+                    log = ' - This email is already registered!';
+                    console.log(body.email + log);
+                    notRegistered = false;
+                    response.redirect("/html/registration.html");
+                    return;
+                }
+
+            }
+
+            fs.appendFile('registrationData.txt', body.email + ' ' + body.username + ' '
+                + body.confirmpassword + ' '
+                + body.firstname + ' ' + body.secondname + ' '
+                + body.city + ' ' + body.state + ' ' + body.subscribe + '\n',
+                function (err) {
+                    if (err) throw err;
+                    console.log(body.email + ' ' + body.username + '\n'
+                        + body.confirmpassword + '\n'
+                        + body.firstname + ' ' + body.secondname + '\n'
+                        + body.city + ' ' + body.state + '\n' + body.subscribe + '\n' + '- registrated!' + '\n');
+                    response.redirect("/html/account.html");
+                });
+
+        });
+    });
 };
